daf7ae2bac2f9a8ce376227e3901084e886c0d8e
sojeong yoon <chaely02@naver.com>
2024-10-17 20:09:02 +0900

    ..

--- src/master.cpp
+++ src/master.cpp
@@ -6,17 +6,14 @@ bool Master::isMyTurn(const Player &player)
   if (player.player_num == this->turn_count % this->player_num)
     is = true;
 
-
-  std::cout<<"this->turn_count"<<this->turn_count<<std::endl;
+  std::cout << "this->turn_count" << this->turn_count << std::endl;
   return is;
 }
-
 
 void Master::timer_callback()
 {
 
   this->timer_->cancel();
-
 
 #define testmode 1
 
@@ -25,11 +22,14 @@ void Master::timer_callback()
   {
     if (isMyTurn(robot)) // 내 턴인 경우
     {
-      RCLCPP_INFO(this->get_logger(), "myturn...999");
+      RCLCPP_INFO(this->get_logger(), "myturn...");
       // 윷 정리 //윷 위치 받아오기.(4개 다? 부딪히면 난리나니까 걍 4번 반복이 나을 듯)//위치 이동, 모션 실행 //
-      //cleanUpBoard();
+      // cleanUpBoard();
       // 윷을 던지기 (잡기)
-      //throwYut();
+
+      RCLCPP_INFO(this->get_logger(), "Throw Yut!");
+      throwYut();
+
       // 윷의 상태 확인(결과)
       int yut_state;
       if (testmode)
@@ -43,7 +43,7 @@ void Master::timer_callback()
       }
 
       // 상대 말판의 상태 확인 //player board update
-      //seeOtherBoard();
+      // seeOtherBoard();
       // 내 보드 보고 업데이트 잡혔을 때를 대비
       // seeMyBoard();
 
@@ -52,15 +52,15 @@ void Master::timer_callback()
       int next_pos;
       move_index = boardAnalysis(yut_state, next_pos);
 
-
       // 로봇 말 이동 내 보드 상태 업데이트
+      RCLCPP_INFO(this->get_logger(), "movePieces");
       movePieces(move_index, next_pos);
 
       // 윷과 모가 나오지 않았다면 턴을 종료
       if (yut_state != 5 && yut_state != 4)
       {
-        //send_request_say_board_state();
-        //send_request_say_special_state(std::string("I'm done with my turn. Send me something to say"));
+        // send_request_say_board_state();
+        // send_request_say_special_state(std::string("I'm done with my turn. Send me something to say"));
         this->turn_count++;
       }
     }
@@ -72,17 +72,16 @@ void Master::timer_callback()
 
     print_dbg();
   }
-  else{
+  else
+  {
     RCLCPP_INFO(this->get_logger(), "game end!");
   }
-
 
   this->timer_->reset();
 }
 
 void Master::playGame()
 {
-
 }
 
 void Master::gameReset()
@@ -139,95 +138,98 @@ void Master::seeOtherBoard()
   }
 }
 
-void Master::seeMyBoard(){
+void Master::seeMyBoard()
+{
 
   std::vector<std::pair<int, int>> redPlayer;
   std::vector<std::pair<int, int>> bluePlayer;
   send_request_board_state(redPlayer, bluePlayer);
 
-  bool index[robot.location.size()] = {false,};
+  bool index[robot.location.size()] = {
+      false,
+  };
 
   if (this->robot.color == "red") // robot이 red면 상대는 blue 는 2
   {
     // 인덱스 맞는거 잡고 안 맞으면 안 맞은 만큼 업뎃 해줘야 겠지...
-    for(size_t i = 0; i < redPlayer.size(); i++)
+    for (size_t i = 0; i < redPlayer.size(); i++)
     {
-      for(size_t j = 0 ; j < robot.location.size(); j++ )
+      for (size_t j = 0; j < robot.location.size(); j++)
       {
-        if(redPlayer[i].first == robot.location[j].first)
+        if (redPlayer[i].first == robot.location[j].first)
         {
           index[j] = true;
           break;
         }
       }
     }
-    //flase 되어있으면 삭제된거임
+    // flase 되어있으면 삭제된거임
     int delete_tokens = 0;
-    for(size_t i = 0; i < robot.location.size(); i++){
-      if(index[i] == false)
+    for (size_t i = 0; i < robot.location.size(); i++)
+    {
+      if (index[i] == false)
       {
         delete_tokens = delete_tokens + robot.location[i].second;
         robot.location.erase(robot.location.begin() + i);
       }
-
     }
-    //삭제된 만큼 채워주기
+    // 삭제된 만큼 채워주기
     for (size_t i = 0; i < delete_tokens; i++)
     {
-      for(int j = -1;  j >= -4 ; j--)
+      for (int j = -1; j >= -4; j--)
       {
-        auto it = std::find_if(robot.location.begin(), robot.location.end(), [j](const std::pair<int, int>& element) {
-        return element.first == j;});
-        if(it == robot.location.end())// 끝까지 안 나옴
+        auto it = std::find_if(robot.location.begin(), robot.location.end(), [j](const std::pair<int, int> &element)
+                               { return element.first == j; });
+        if (it == robot.location.end()) // 끝까지 안 나옴
         {
-          robot.location.push_back(std::make_pair(j,1));
+          robot.location.push_back(std::make_pair(j, 1));
         }
       }
     }
   }
   else
   { // robot이 blue면 상대는 red red는 1
-        // 인덱스 맞는거 잡고 안 맞으면 안 맞은 만큼 업뎃 해줘야 겠지...
-    for(size_t i = 0; i < bluePlayer.size(); i++)
+    // 인덱스 맞는거 잡고 안 맞으면 안 맞은 만큼 업뎃 해줘야 겠지...
+    for (size_t i = 0; i < bluePlayer.size(); i++)
     {
-      for(size_t j = 0 ; j < robot.location.size(); j++ )
+      for (size_t j = 0; j < robot.location.size(); j++)
       {
-        if(bluePlayer[i].first == robot.location[j].first)
+        if (bluePlayer[i].first == robot.location[j].first)
         {
           index[j] = true;
           break;
         }
       }
     }
-    //flase 되어있으면 삭제된거임
+    // flase 되어있으면 삭제된거임
     int delete_tokens = 0;
-    for(size_t i = 0; i < robot.location.size(); i++){
-      if(index[i] == false)
+    for (size_t i = 0; i < robot.location.size(); i++)
+    {
+      if (index[i] == false)
       {
         delete_tokens = delete_tokens + robot.location[i].second;
         robot.location.erase(robot.location.begin() + i);
       }
-
     }
-    //삭제된 만큼 채워주기
+    // 삭제된 만큼 채워주기
     for (size_t i = 0; i < delete_tokens; i++)
     {
-      for(int j = -1;  j >= -4 ; j--)
+      for (int j = -1; j >= -4; j--)
       {
-        auto it = std::find_if(robot.location.begin(), robot.location.end(), [j](const std::pair<int, int>& element) {
-        return element.first == j;});
-        if(it == robot.location.end())// 끝까지 안 나옴
+        auto it = std::find_if(robot.location.begin(), robot.location.end(), [j](const std::pair<int, int> &element)
+                               { return element.first == j; });
+        if (it == robot.location.end()) // 끝까지 안 나옴
         {
-          robot.location.push_back(std::make_pair(j,1));
+          robot.location.push_back(std::make_pair(j, 1));
         }
       }
     }
   }
-
 }
 
 void Master::throwYut()
 {
+  RCLCPP_INFO(this->get_logger(), "in throwYut");
   send_request_yut_throw();
 }
 
@@ -247,19 +249,19 @@ void Master::movePieces(int piece_index, int next_pos)
   int current_pos = robot.location[piece_index].first;
   int current_tokens = robot.location[piece_index].second;
   robot.location.erase(robot.location.begin() + piece_index);
-  //print_dbg();
+  // print_dbg();
 
-  std::cout<<" move : "<<current_pos<<"next : "<<next_pos<<std::endl;
+  std::cout << " move : " << current_pos << "next : " << next_pos << std::endl;
   float place_z = 0;
   const float height = 10;
   bool overlap = false;
-  for (int i = 0; i < robot.location.size(); i++)
+  for (size_t i = 0; i < robot.location.size(); i++)
   { // 겹치는 값이 있는지 확인 하여 z 값 변화.
 
     if (robot.location[i].first == next_pos)
     {
 
-      std::cout<<"nononononononoon"<<std::endl;
+      std::cout << "nononononononoon" << std::endl;
 
       robot.location[i].second++;
       place_z = (robot.location[i].second - 1) * height; //
@@ -271,15 +273,15 @@ void Master::movePieces(int piece_index, int next_pos)
   if (!overlap)
   {
 
-    std::cout<<"lakdjfklasdjfkljaslkdfjlasf"<<std::endl;
+    std::cout << "lakdjfklasdjfkljaslkdfjlasf" << std::endl;
     robot.location.push_back(std::make_pair(next_pos, current_tokens));
   }
 
-  if(next_pos == 30){// 도착했다면
-    robot.finish_pieces +=  current_tokens;
-
+  if (next_pos == 30)
+  { // 도착했다면
+    robot.finish_pieces += current_tokens;
   }
-  //pickAndPlace(current_pos, next_pos, place_z);
+  pickAndPlace(current_pos, next_pos, place_z);
 }
 void Master::boardUpDate()
 {
@@ -304,7 +306,7 @@ int Master::boardAnalysis(int current_yut, int &next_pos)
   int new_pos[robot.location.size()]; // newpos
   const int new_pos_size = robot.location.size();
 
-  for (int i = 0; i < robot.location.size(); ++i) // 말의 다음위치 구하기 현재 위치가 지름길 위치인지? 지름길 위치라면 들어간 걸로 숫자 바꿔주기 아니라면 그냥 더하기
+  for (size_t i = 0; i < robot.location.size(); ++i) // 말의 다음위치 구하기 현재 위치가 지름길 위치인지? 지름길 위치라면 들어간 걸로 숫자 바꿔주기 아니라면 그냥 더하기
   {
     int current_pos = robot.location[i].first;   // index
     if (robot.short_cut_index[0] == current_pos) // 5
@@ -334,7 +336,7 @@ int Master::boardAnalysis(int current_yut, int &next_pos)
       }
     }
   }
-  std::cout<<"aaaa"<<std::endl;
+  std::cout << "aaaa" << std::endl;
 
   int max_tokens = -1;
   int min_left = 100;
@@ -370,11 +372,11 @@ int Master::boardAnalysis(int current_yut, int &next_pos)
       }
     }
   }
-  std::cout<<"bbbb"<<std::endl;
+  std::cout << "bbbb" << std::endl;
   if (Arrivalflag)
   {
     next_pos = 30;
-    //!send_request_say_special_state(std::string("My token has reached the destination, and I earned points!"));
+    //! send_request_say_special_state(std::string("My token has reached the destination, and I earned points!"));
     return index;
   };
 
@@ -393,9 +395,9 @@ int Master::boardAnalysis(int current_yut, int &next_pos)
   }
 
   // 3. 잡을 수 있는 말이 있는지 확인 (현재 위치에 상대방 말이 있는지)
-  for (int i = 0; i < robot.location.size(); i++)
+  for (size_t i = 0; i < robot.location.size(); i++)
   {
-    for (int k = 0; k < player.location.size(); ++k)
+    for (size_t k = 0; k < player.location.size(); ++k)
     {
       if (player.location[k].first == new_pos[i]) // 같은 위치에 상대 말이 있는지 확인
       {
@@ -411,7 +413,7 @@ int Master::boardAnalysis(int current_yut, int &next_pos)
   }
 
   // 4. 지금길로 들어갈 수 있는지 확인
-  for (int i = 0; i < robot.location.size(); ++i)
+  for (size_t i = 0; i < robot.location.size(); ++i)
   {
     const int shortcutsize = 4;
     for (int j = 0; j < shortcutsize; j++)
@@ -427,7 +429,7 @@ int Master::boardAnalysis(int current_yut, int &next_pos)
   // 5. 가장 멀리 가 있는 애 보내기
   int farindex = -10;
   int farmax = -10;
-  for (int i = 0; i < robot.location.size(); ++i)
+  for (size_t i = 0; i < robot.location.size(); ++i)
   {
     if (farmax < robot.location[i].first)
     {
@@ -442,8 +444,12 @@ int Master::boardAnalysis(int current_yut, int &next_pos)
 // pick place 시키는 함수
 void Master::pickAndPlace(float x, float y, float z, float rotate, float x2, float y2, float z2, float rotate2)
 {
+
   std::array<float, 4> pick_pos = {x, y, z, rotate};
   std::array<float, 4> place_pos = {x2, y2, z2, rotate2};
+
+  std::cout << "zzzzz2222" << z << std::endl;
+
   send_request_pick_place(pick_pos, place_pos);
 }
 
@@ -453,8 +459,23 @@ void Master::pickAndPlace(int pick_index, int place_index, float place_z)
   if (place_index > 30)
     place_index = 30;
 
-  std::tuple<float, float, float, float> pick = robot.board_coor.at(pick_index);
-  std::tuple<float, float, float, float> place = robot.board_coor.at(place_index);
+  auto pick_iter = robot.board_coor.find(pick_index);
+  auto place_iter = robot.board_coor.find(place_index);
+
+  // 맵에서 찾았는지 확인
+  // if (pick_iter != robot.board_coor.end() && place_iter != robot.board_coor.end())
+  // {
+  std::tuple<float, float, float, float> pick = pick_iter->second;
+  std::tuple<float, float, float, float> place = place_iter->second;
+
+  // 이후 pick과 place 사용
+  //}
+
+  // std::tuple<float, float, float, float> pick = robot.board_coor.find(pick_index);
+  // std::tuple<float, float, float, float> place = robot.board_coor.find(place_index);
+
+  std::cout << "zzzzz" << std::get<2>(pick) << std::endl;
+  std::cout << "zz" << std::get<2>(place) << std::endl;
 
   pickAndPlace(std::get<0>(pick), std::get<1>(pick), std::get<2>(pick), std::get<3>(pick),
                std::get<0>(place), std::get<1>(place), std::get<2>(place) + place_z, std::get<3>(place));
@@ -522,41 +543,41 @@ void Master::send_request_pick_place(const std::array<float, 4> &pick_pos, const
   // 비동기 요청 보내고 응답을 기다림
   auto result_future = client_pickplace->async_send_request(request);
 
-  // 응답이 올 때까지 대기
-  if (rclcpp::spin_until_future_complete(this->get_node_base_interface(), result_future) ==
-      rclcpp::FutureReturnCode::SUCCESS)
-  {
-    // 응답 받음
-    auto response = result_future.get();
+  // // 응답이 올 때까지 대기
+  // if (rclcpp::spin_until_future_complete(this->get_node_base_interface(), result_future) ==
+  //     rclcpp::FutureReturnCode::SUCCESS)
+  // {
+  //   // 응답 받음
+  //   auto response = result_future.get();
 
-    // 결과 확인 및 처리
-    if (response->result)
-    {
-      RCLCPP_INFO(this->get_logger(), "Pick and place operation succeeded.");
-    }
-    else
-    {
-      RCLCPP_WARN(this->get_logger(), "Pick and place operation failed. Failure code: %d", response->failure_code);
-      // 실패 코드에 따른 처리 (예시)
-      if (response->failure_code == 1)
-      {
-        RCLCPP_WARN(this->get_logger(), "Impossible to pick.");
-      }
-      else if (response->failure_code == 2)
-      {
-        RCLCPP_WARN(this->get_logger(), "Impossible to place.");
-      }
-      else if (response->failure_code == 3)
-      {
-        RCLCPP_WARN(this->get_logger(), "No object to pick.");
-      }
-    }
-  }
-  else
-  {
-    // 서비스 호출 실패
-    RCLCPP_ERROR(this->get_logger(), "Failed to call service pick_and_place.");
-  }
+  //   // 결과 확인 및 처리
+  //   if (response->result)
+  //   {
+  //     RCLCPP_INFO(this->get_logger(), "Pick and place operation succeeded.");
+  //   }
+  //   else
+  //   {
+  //     RCLCPP_WARN(this->get_logger(), "Pick and place operation failed. Failure code: %d", response->failure_code);
+  //     // 실패 코드에 따른 처리 (예시)
+  //     if (response->failure_code == 1)
+  //     {
+  //       RCLCPP_WARN(this->get_logger(), "Impossible to pick.");
+  //     }
+  //     else if (response->failure_code == 2)
+  //     {
+  //       RCLCPP_WARN(this->get_logger(), "Impossible to place.");
+  //     }
+  //     else if (response->failure_code == 3)
+  //     {
+  //       RCLCPP_WARN(this->get_logger(), "No object to pick.");
+  //     }
+  //   }
+  // }
+  // else
+  // {
+  //   // 서비스 호출 실패
+  //   RCLCPP_ERROR(this->get_logger(), "Failed to call service pick_and_place.");
+  // }
 }
 
 //
@@ -572,11 +593,11 @@ void Master::send_request_board_state(std::vector<std::pair<int, int>> &player1,
     // 응답 받음
     auto response = result_future.get();
     // 결과 확인 및 처리
-    for (int i = 0; i < response->positions_p1.size(); i++)
+    for (size_t i = 0; i < response->positions_p1.size(); i++)
     {
       player1.push_back(std::make_pair(response->positions_p1[i], response->tokens_p1[i]));
     }
-    for (int i = 0; i < response->positions_p2.size(); i++)
+    for (size_t i = 0; i < response->positions_p2.size(); i++)
     {
       player2.push_back(std::make_pair(response->positions_p2[i], response->tokens_p2[i]));
     }
@@ -587,29 +608,49 @@ void Master::send_request_board_state(std::vector<std::pair<int, int>> &player1,
 void Master::send_request_yut_throw()
 {
 
+  RCLCPP_INFO(this->get_logger(), "Success send_request_yut_throw");
+
   // 요청 객체 생성
   auto request = std::make_shared<xyz_interfaces::srv::YutThrow::Request>();
   // request->none = true;
+  RCLCPP_INFO(this->get_logger(), "Success send_request_yut_throw1");
 
   // 비동기 요청 보내고 응답을 기다림
   auto result_future = client_throw->async_send_request(request);
 
+  RCLCPP_INFO(this->get_logger(), "Success send_request_yut_throw2");
+
   // 응답이 올 때까지 대기
-  if (rclcpp::spin_until_future_complete(this->get_node_base_interface(), result_future) ==
-      rclcpp::FutureReturnCode::SUCCESS)
-  {
-    // 응답 받음
-    auto response = result_future.get();
-    if (response->success)
-    {
-      RCLCPP_ERROR(this->get_logger(), "Success Throw yut");
-    }
-  }
-  else
-  {
-    // 서비스 호출 실패
-    RCLCPP_ERROR(this->get_logger(), "Failed to throw.");
-  }
+  // 스핀 함수가 중복되지 않도록 코드 수정
+
+  // if (rclcpp::spin_until_future_complete(this->get_node_base_interface(), result_future) ==
+  //   rclcpp::FutureReturnCode::SUCCESS)
+  // {
+
+  //   RCLCPP_INFO(this->get_logger(), "Success send_request_yut_throw3");
+
+  // }
+
+  //  rclcpp::executors::SingleThreadedExecutor executor;
+  //   executor.add_node(this->shared_from_this());  // 노드를 추가
+  //   if (executor.spin_until_future_complete(result_future) == rclcpp::FutureReturnCode::SUCCESS)
+  //   {
+
+  //     RCLCPP_INFO(this->get_logger(), "Success send_request_yut_throw3");
+  //     // 응답 받음
+  //     auto response = result_future.get();
+  //     if (response->success)
+  //     {
+  //       RCLCPP_ERROR(this->get_logger(), "Success Throw yut");
+  //     }
+  //   }
+  //   else
+  //   {
+  //     // 서비스 호출 실패
+  //     RCLCPP_ERROR(this->get_logger(), "Failed to throw.");
+  //   }
+
+  RCLCPP_INFO(this->get_logger(), "Success send_request_yut_throw4");
 }
 
 void Master::send_request_yut_pick(float x, float y, float z, float r)
@@ -619,28 +660,29 @@ void Master::send_request_yut_pick(float x, float y, float z, float r)
   auto request = std::make_shared<xyz_interfaces::srv::YutPick::Request>();
   std::array<float, 4> pickpos = {x, y, z, r};
   request->pick_pos = pickpos;
+  request->place_num = 0;
 
   // 비동기 요청 보내고 응답을 기다림
   auto result_future = client_pick_put->async_send_request(request);
 
-  // 응답이 올 때까지 대기
-  if (rclcpp::spin_until_future_complete(this->get_node_base_interface(), result_future) ==
-      rclcpp::FutureReturnCode::SUCCESS)
-  {
-    // 응답 받음
-    auto response = result_future.get();
+  // // 응답이 올 때까지 대기
+  // if (rclcpp::spin_until_future_complete(this->get_node_base_interface(), result_future) ==
+  //     rclcpp::FutureReturnCode::SUCCESS)
+  // {
+  //   // 응답 받음
+  //   auto response = result_future.get();
 
-    // 결과 확인 및 처리
-    if (response->result)
-    {
-      RCLCPP_INFO(this->get_logger(), "Pick and place operation succeeded.");
-    }
-  }
-  else
-  {
-    // 서비스 호출 실패
-    RCLCPP_ERROR(this->get_logger(), "Failed to call service pick_and_place.");
-  }
+  //   // 결과 확인 및 처리
+  //   if (response->result)
+  //   {
+  //     RCLCPP_INFO(this->get_logger(), "Pick and place operation succeeded.");
+  //   }
+  // }
+  // else
+  // {
+  //   // 서비스 호출 실패
+  //   RCLCPP_ERROR(this->get_logger(), "Failed to call service pick_and_place.");
+  // }
 }
 
 void Master::send_request_say_board_state()
@@ -665,23 +707,23 @@ void Master::send_request_say_board_state()
   auto result_future = client_say_board->async_send_request(request);
 
   // 응답이 올 때까지 대기
-  if (rclcpp::spin_until_future_complete(this->get_node_base_interface(), result_future) ==
-      rclcpp::FutureReturnCode::SUCCESS)
-  {
-    // 응답 받음
-    auto response = result_future.get();
+  // if (rclcpp::spin_until_future_complete(this->get_node_base_interface(), result_future) ==
+  //     rclcpp::FutureReturnCode::SUCCESS)
+  // {
+  //   // 응답 받음
+  //   auto response = result_future.get();
 
-    // 결과 확인 및 처리
-    if (response->success)
-    {
-      RCLCPP_INFO(this->get_logger(), "send_request_say_board_state operation succeeded.");
-    }
-  }
-  else
-  {
-    // 서비스 호출 실패
-    RCLCPP_ERROR(this->get_logger(), "Failed to call service send_request_say_board_state.");
-  }
+  //   // 결과 확인 및 처리
+  //   if (response->success)
+  //   {
+  //     RCLCPP_INFO(this->get_logger(), "send_request_say_board_state operation succeeded.");
+  //   }
+  // }
+  // else
+  // {
+  //   // 서비스 호출 실패
+  //   RCLCPP_ERROR(this->get_logger(), "Failed to call service send_request_say_board_state.");
+  // }
 }
 void Master::send_request_say_special_state(const std::string &say)
 {
